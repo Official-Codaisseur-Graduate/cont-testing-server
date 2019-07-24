69e5e082365671267bce90aec22116038777ee6e
coderhook <abel1987@hotmail.es>
2019-07-24 11:27:54 +0200

    add questions passed by student by Id

--- evaluations/routes.js
+++ evaluations/routes.js
@@ -352,9 +352,31 @@ router.post('/evaluations', (req, res, next) => {
     })
 })
 
-router.get('/evaluations-by-question-student', (req, res, next) => {
+router.get('/evaluations-by-question-student/:range', (req, res, next) => {
+
+   // Get Date Range interested in.
+   const rangeDate = req.params.range 
+   console.log('THIS RANGE!!!!!!!!!!!!!!!!', rangeDate)
+   const currentDate = new Date()
+   const selectedRange = new Date()
+ 
+   const range = {
+     'today': 0,
+     'lastWeek': 7,
+     'lastMonth': 30,
+     'lastYear': 365,
+     'allData': 365*5
+   }
+  
+   selectedRange.setDate(selectedRange.getDate() - range[rangeDate]);
+
   Evaluation
     .findAll({
+      where: {
+        createdAt: {
+          [Op.gt]: selectedRange
+        }
+      },
       attributes: [['studentId', 'studentId'], ['questionId', 'questionId']],
       group: ['studentId', 'questionId'],
       order: [['studentId', 'ASC'], ['questionId', 'ASC'],],
